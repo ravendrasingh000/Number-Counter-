3a322d8b6a583ac01b06fb20368c4127b8b61de3
Ravendra Singh <ravendra24@navgurukul.org>
2025-01-14 08:54:59 +0530

    initial

---
+++ script.js
@@ -0,0 +1,31 @@
+const changeCount = document.querySelector("#change-count");
+const btn = document.querySelectorAll("button");
+let current = 0;
+
+function updateDisplay() {
+    if (current > 0) {
+        changeCount.style.color = "blue";
+    } else if (current < 0) {
+        changeCount.style.color = "red";
+    } else {
+        changeCount.style.color = "black";
+    }
+}
+
+btn[0].addEventListener("click",()=> {
+    current  -= 1;
+    changeCount.innerText = current;
+    updateDisplay()
+})
+
+btn[2].addEventListener("click",()=> {
+    current  += 1;
+    changeCount.innerText = current;
+    updateDisplay()
+})
+
+btn[1].addEventListener("click", ()=> {
+    changeCount.innerText = 0;
+    updateDisplay()
+})  
+
